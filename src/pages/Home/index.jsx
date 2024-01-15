f48b88af8a510e8c75150d98de4a103d0f69528f
Flávio <f.augustosdn@gmail.com>
2024-01-15 12:56:13 -0300

    feat: finalizada estilização do header e definidas as fontes dos textos do projeto

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,18 @@
-import { Container, Header, Main, Footer } from "./styles";
+import { Container, Header, Main, Footer, CameraIcon } from "./styles";
+import Perfil from "../../assets/perfil.jpg";
 
 export function Home() {
   return (
     <Container>
-      <Header></Header>
+      <Header>
+        <CameraIcon />
+        <ul>
+          <li>Home</li>
+          <li>Galeria</li>
+        </ul>
+        <input type="text" placeholder="Pesquise por imagens e coleções" />
+        <img src={Perfil} alt="Imagem de uma pessoa com barba, cabelo cortado e olhos castanhos" />
+      </Header>
       <Main></Main>
       <Footer></Footer>
     </Container>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,9 +1,49 @@
 import styled from "styled-components";
+import { FaCamera } from "react-icons/fa";
 
 export const Container = styled.div``;
 
-export const Header = styled.header``;
+export const Header = styled.header`
+  background-color: #fff;
+  height: 8rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  gap: 2rem;
+
+  ul {
+    display: flex;
+    list-style: none;
+    gap: 7.5rem;
+
+    li {
+      font-size: 2rem;
+      line-height: 2.88rem;
+      cursor: pointer;
+    }
+  }
+
+  input {
+    width: 71rem;
+    padding: 1.2rem 1.6rem;
+    outline: none;
+
+    border: none;
+    border-radius: 0.5rem;
+    background-color: #eee;
+  }
+
+  img {
+    height: 4.2rem;
+    width: 4.2rem;
+    border-radius: 50%;
+  }
+`;
 
 export const Main = styled.main``;
 
 export const Footer = styled.footer``;
+
+export const CameraIcon = styled(FaCamera)`
+  font-size: 3.5rem;
+`;

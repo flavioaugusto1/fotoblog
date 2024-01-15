93cef2774e440237b627cbe27c4b9b0d12d6f07f
Flávio <f.augustosdn@gmail.com>
2024-01-15 11:53:57 -0300

    feat: definida estrutura da página de home

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import { Container, Header, Main, Footer } from "./styles";
+
+export function Home() {
+  return (
+    <Container>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </Container>
+  );
+}
